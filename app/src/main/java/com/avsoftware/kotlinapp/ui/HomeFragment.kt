8e839eec26fe9a54778780790680db8430b7c43d
Andrew Bennett <abennett@safetonet.com>
2018-06-23 10:23:49 +0100

    added firebase and crashlytics config

--- app/src/main/java/com/avsoftware/kotlinapp/ui/HomeFragment.kt
+++ app/src/main/java/com/avsoftware/kotlinapp/ui/HomeFragment.kt
@@ -22,7 +22,7 @@ class HomeFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
 
-        viewBinding = HomeFragmentBinding.inflate(LayoutInflater.from(context))
+        viewBinding = HomeFragmentBinding.inflate(inflater)
 
         return viewBinding.root
     }
